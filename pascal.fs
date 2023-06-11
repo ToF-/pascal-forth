0c8e8db3c3fd9adf018e9ec63a776e8a80ef9f78
Christophe Thibaut <cthibaut@octo.com>
2023-06-11 18:30:53 +0200

    refactoring

--- pascal.fs
+++ pascal.fs
@@ -27,11 +27,17 @@ VARIABLE NEXT-TERM-ADDR
 : SUM ( n -- n )
   DUP IF 1+ 0 SWAP 1 DO I + LOOP THEN ;
 
-: .LINE ( addr,n -- )
+: .TERMS ( addr,n -- )
   0 DO DUP I CELLS + @ . LOOP CR DROP ;
 
+: LINE-ADDR ( addr,n -- addr )
+  SUM CELLS + ;
+
+: .LINE ( addr,n -- )
+    TUCK LINE-ADDR SWAP 1+ .TERMS ;
+
 : .LINES ( addr,n -- ) 
-  0 DO I SUM CELLS OVER + I 1+ .LINE LOOP DROP ;
+  0 DO DUP I .LINE LOOP DROP ;
 
 CREATE TRIANGLE 20 SUM CELLS ALLOT
 TRIANGLE NEXT-TERM-ADDR !
